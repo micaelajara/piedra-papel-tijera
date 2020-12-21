ed672f970fc074890e629b890a4e9d94375ef3ba
micaelajara <micaaelajara@gmail.com>
2020-12-21 17:31:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const imgPlayerChoice = document.getElementById("playerChoice");
+const imgComputerChoice = document.getElementById("computerChoice");
+
+const pResult = document.getElementById("result");
+const pScore = document.getElementById("score");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(function(button){
+button.addEventListener("Click", startGame)
+console.log("Ejecutó el ForEach")
+})
+function startGame(event){
+    console.log("click")
+}
